2296d7144c2e9bd7acfed4ac9294554c810fd554
ShiNLea <LeafShinobi319@gmail.com>
2024-04-26 11:40:01 -0600

    Update pocketbase.js

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -4,7 +4,7 @@ import PocketBase from 'pocketbase';
 import { writable } from "svelte/store";
 
 // I don't have an IP yet because I don't wanna pay or deploy yet lol
-export const pb = new PocketBase('http://IPADDRESSGOESHERE');
+export const pb = new PocketBase('http://127.0.0.1:8090');
 
 // currentUser = null when not logged in, = database record when logged in
 export const currentUser = writable(pb.authStore.model);
