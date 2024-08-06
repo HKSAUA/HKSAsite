8f904f47dce387ada08e1b53b9c20999af890358
Ryan <58052631+ShiNLea@users.noreply.github.com>
2024-08-06 15:50:07 -0600

    please work

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -3,7 +3,6 @@ import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private'
 
 console.log("HEP")
 const supabaseUrl = SUPABASE_URL;
-console.log(supabaseUrl);
 const supabaseKey = SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
