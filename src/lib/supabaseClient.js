230215d748cd9e65a72170e6b68cae0cd37463b6
Ryan <58052631+ShiNLea@users.noreply.github.com>
2024-08-07 08:58:12 -0600

    cleaning up, pray for a working pr

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private'
 
-console.log("HEP")
 const supabaseUrl = SUPABASE_URL;
 const supabaseKey = SUPABASE_ANON_KEY;
 
