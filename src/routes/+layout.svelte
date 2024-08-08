d42e081204556b1703805d863e7f025d5f6acba3
Ryan <58052631+ShiNLea@users.noreply.github.com>
2024-08-08 10:27:45 -0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -41,12 +41,13 @@
 
 	main {
 		flex: 1;
-		
-
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
 		width: 100%;
-		text-align: center;
+		max-width: 64rem;
 		margin: 0 auto;
-
+		box-sizing: border-box;
 	}
 
 	.has-words {
