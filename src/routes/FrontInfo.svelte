81b41f491cdd2f6d0c3a6ff48e2339bd750c4ed7
Ryan <58052631+ShiNLea@users.noreply.github.com>
2024-08-12 14:44:18 -0600

    setting up dynamic routing for events

--- src/routes/FrontInfo.svelte
+++ src/routes/FrontInfo.svelte
@@ -1,6 +1,6 @@
 <div class="with-image">
     <h2>About HKSAUA</h2>
     The Hong Kong Students' Association at the University of Alberta (HKSAUA) provides a vibrant community for students across campus.
-			Through cultural events, social gatherings, and much more, HKSAUA fosters a welcoming environment where
-			members can conenct, learn, and share experiences.
+    Through cultural events, social gatherings, and much more, HKSAUA fosters a welcoming environment where
+    members can conenct, learn, and share experiences.
 </div>
