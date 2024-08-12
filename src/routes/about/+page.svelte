c03c3e929cb12e80a05eb9f60a6963f0fa9440e2
Ryan <58052631+ShiNLea@users.noreply.github.com>
2024-08-12 15:31:24 -0600

    lotsa pre-planning

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import ExecCard from "./ExecCard.svelte";
-	import {currentUser} from "$lib/pocketbase.js";
+
 	import Preview from "../Preview.svelte";
 </script>
 
@@ -14,7 +14,7 @@
 <body>
 	<div class="text-column" style="text-align:center">
 		<h1>About HKSA (wip lol)</h1>
-		{#if $currentUser}
+
 		<p>
 			The Hong Kong Students' Association at the University of Alberta (HKSAUA) provides a vibrant community for students across campus.
 			Through cultural events, social gatherings, and much more, HKSAUA fosters a welcoming environment where
@@ -24,9 +24,9 @@
 		<div>
 			<h3>Executives</h3>
 		</div>
-		{/if}
+
 	</div>
 	<ExecCard />
-	<Preview/>
+
 </body>
 
--- src/routes/about/ExecCard.svelte
+++ src/routes/about/ExecCard.svelte
@@ -30,7 +30,6 @@
             </div>
         {/each}
     </div>
-
 </div>
 
 
